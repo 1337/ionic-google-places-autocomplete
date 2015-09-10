423106e544e98396bd1c48789ef0accc469262c0
Brian Lai <brian@rideco.com>
2015-09-10 10:57:07 -0400

    grunt version bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,60 +1,82 @@
 /*
- * angular-google-places-autocomplete
+ * ionic-google-places-autocomplete
  *
  * Copyright (c) 2014 "kuhnza" David Kuhn
  * Licensed under the MIT license.
- * https://github.com/kuhnza/angular-google-places-autocomplete/blob/master/LICENSE
+ * https://github.com/kuhnza/ionic-google-places-autocomplete/blob/master/LICENSE
  */
+(function () {
+    "use strict";
 
-'use strict';
+    module.exports = function (grunt) {
+        // Load grunt tasks automatically
+        require('load-grunt-tasks')(grunt);
 
-module.exports = function (grunt) {
-	// Load grunt tasks automatically
-	require('load-grunt-tasks')(grunt);
+        var pixrem = require('pixrem');
+        var autoprefixer = require('autoprefixer');
+        var cssnano = require('cssnano');
 
-	grunt.initConfig({
-		karma: {
-			unit: {
-				configFile: 'karma.conf.js',
-				singleRun: true
-			}
-		},
-		clean: {
-			dist: { src: 'dist', dot: true },
-            lib: { src: 'example/lib', dot: true },
-			bower: { src: 'bower_components', dot: true }
-		},
-		bower: { 
-			install: { options: { targetDir: 'example/lib' } } 
-		},
-        cssmin: {
-            dist: {
-                expand: true,
-                cwd: 'dist/',
-                files: {
-                    'dist/autocomplete.min.css': 'src/autocomplete.css'
+        grunt.loadNpmTasks('grunt-postcss');
+
+        grunt.initConfig({
+            karma: {
+                unit: {
+                    configFile: 'karma.conf.js',
+                    singleRun: true
+                }
+            },
+            clean: {
+                dist: {src: 'dist', dot: true},
+                bower: {src: 'bower_components', dot: true}
+            },
+            bower: {
+                install: {options: {targetDir: 'example/lib'}}
+            },
+            cssmin: {
+                dist: {
+                    expand: true,
+                    cwd: 'dist/',
+                    files: {
+                        'dist/autocomplete.min.css': 'src/autocomplete.css'
+                    }
+                }
+            },
+            postcss: {
+                options: {
+                    map: true, // inline sourcemaps
+                    processors: [
+                        pixrem(), // add fallbacks for rem units
+                        autoprefixer({
+                            browsers: 'last 2 versions'
+                        }), // add vendor prefixes
+                        cssnano()  // minify the result
+                    ]
+                },
+                dist: {
+                    src: 'css/*.css'
+                }
+            },
+            uglify: {
+                dist: {
+                    files: {
+                        'dist/autocomplete.min.js': 'src/autocomplete.js'
+                    }
                 }
             }
-        },
-		uglify: {
-			dist: {
-				files: {
-					'dist/autocomplete.min.js': 'src/autocomplete.js'
-				}
-			}
-		}
-	});
+        });
 
-	grunt.registerTask('test', [
-		'karma'
-	]);
+        grunt.registerTask('test', [
+            'karma'
+        ]);
 
-	grunt.registerTask('build', [
-		'clean',
-		'bower',
-        'cssmin',
-		'uglify'
-	]);
+        grunt.registerTask('build', [
+            'clean',
+            'bower',
+            'cssmin',
+            'uglify'
+        ]);
 
-	grunt.registerTask('default', ['build']);
-};
+        grunt.registerTask('default', ['build']);
+    };
+
+}());
